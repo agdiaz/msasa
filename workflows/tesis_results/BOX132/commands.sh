24fd59534199acfb4fdf3685a3b0706b2d364c10
Adrián Diaz <diaz.adrian.g@gmail.com>
2023-02-14 15:43:14 +0100

    Add new results

---
+++ workflows/tesis_results/BOX132/commands.sh
@@ -0,0 +1,8 @@
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_clustal.msf > BOX132_balibase_clustal.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_kalign.msf > BOX132_balibase_kalign.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_mafft.msf > BOX132_balibase_mafft.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_msasa_blosum.msf > BOX132_balibase_msasa_blosum.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_msasa_singlematching.msf > BOX132_balibase_msasa_singlematching.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_msasa_singlems.msf > BOX132_balibase_msasa_singlems.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_muscle.msf > BOX132_balibase_muscle.txt
+/baliscore/bali_score dataset_testing/BOX132.xml workflows/tesis_results/BOX132/BOX132_alignment_tCoffee.msf > BOX132_balibase_tCoffee.txt
---
+++ workflows/tesis_results/BOX212/commands.sh
@@ -0,0 +1,8 @@
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_clustal.msf > BOX212_balibase_clustal.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_kalign.msf > BOX212_balibase_kalign.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_mafft.msf > BOX212_balibase_mafft.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_msasa_blosum.msf > BOX212_balibase_msasa_blosum.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_msasa_singlematching.msf > BOX212_balibase_msasa_singlematching.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_msasa_singlems.msf > BOX212_balibase_msasa_singlems.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_muscle.msf > BOX212_balibase_muscle.txt
+/baliscore/bali_score dataset_testing/BOX212.xml workflows/tesis_results/BOX212/BOX212_alignment_tCoffee.msf > BOX212_balibase_tCoffee.txt
